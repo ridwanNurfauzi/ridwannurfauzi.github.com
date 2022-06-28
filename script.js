afdd2f6c9c28f06218528a77f3998c22d925bd6f
ridwanNurfauzi <mrnhp123@gmail.com>
2022-06-28 17:55:50 +0700

    Edit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function onReady(cb) {
     let intervalId = window.setInterval(() => {
+        document.querySelector("article").style.visibility = 'hidden';
         if (document.getElementsByTagName('body')[0] !== undefined) {
             window.clearInterval(intervalId);
             cb.call(this);
@@ -9,4 +10,5 @@ function onReady(cb) {
 onReady(() => {
     document.querySelector('.loading').style.visibility = 'hidden';
     document.querySelector('.loading').style.animation = 'loading-last-animation 1s';
+    document.querySelector("article").style.visibility = 'visible';
 });
